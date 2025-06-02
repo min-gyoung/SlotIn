b988af4c97ae6e18edf6f5393dafa627ca428e4c
Bora <b0rayun@sookmyung.ac.kr>
2025-06-02 16:46:00 +0900

    feat: ContentView TabBar 구현

--- SlotIn/Views/Home/SelectTask.swift
+++
@@ -1,8 +0,0 @@
-//
-//  SelectTask.swift
-//  SlotIn
-//
-//  Created by 김민경 on 6/2/25.
-//
-
-import Foundation
---
+++ SlotIn/Views/Home/SelectTaskView.swift
@@ -0,0 +1,19 @@
+//
+//  SelectTaskView.swift
+//  SlotIn
+//
+//  Created by 김민경 on 6/2/25.
+//
+
+import SwiftUI
+
+struct SelectTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectTaskView()
+}
+
