a9f735d15cd7d8025e976381e09d7a8bc45b1275
Bora <b0rayun@sookmyung.ac.kr>
2025-06-04 20:20:27 +0900

    feature: TaskDetailView 리스트 구현

--- SlotIn/Views/Task/TaskDetailView.swift
+++ SlotIn/Views/Task/TaskDetailView.swift
@@ -8,15 +8,60 @@
 import SwiftUI
 
 struct TaskDetailView: View {
+    
+    @Environment(\.dismiss) private var dismiss
+    
     var body: some View {
         ZStack{
             Color.gray700.ignoresSafeArea()
-            VStack{
-                Text("작업 세부 정보")
-                    .foregroundColor(.gray100)
+            VStack(alignment:.leading){
+                //1. Button : back button
+                HStack{
+                    Button{
+                        dismiss()
+                    } label:{
+                        Image(systemName: "chevron.left")
+                            .foregroundColor(.green100)
+                            .font(.system(size:22))
+                    }
+                   
+                    Text("보관함")
+                        .font(.system(size:17))
+                    Spacer()
+                }.foregroundColor(.green100).padding(8)
+                //2. header: 작업 세부 정보
+                HStack{
+                    Text("작업 세부 정보")
+                        .foregroundColor(.gray100)
+                        .font(.system(size: 28, weight: .bold))
+                    Spacer()
+                }.padding(16)
+                
+                //3. Content
+                
+                VStack(alignment: .leading){
+                    Text("제목")
+                        .font(.system(size: 15))
+                        .foregroundColor(.gray300)
+                        .padding(.horizontal,15)
+                        
+                    
+                    Text("서강대학교 홍보영상 기획 회의")
+                        .foregroundColor(.white)
+                        .padding(.horizontal,16)
+                        .font(.system(size:16, weight: .semibold))
+                        .frame(width: 368, height: 44, alignment: .leading)
+                        .background(.gray600)
+                        .cornerRadius(8)
+                        
+                }.padding(.horizontal,13)
+                
+                
+                //4. Button
+                
                 
             }
-        }
+        }.navigationBarBackButtonHidden()
     }
 }
 
