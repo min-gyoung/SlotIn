5a7ef1d65a2027dc4967f35b375ebb7f70f9ef85
Bora <b0rayun@sookmyung.ac.kr>
2025-06-07 00:41:24 +0900

    fix: conflict fix

--- SlotIn/Views/Task/TaskDetailView.swift
+++ SlotIn/Views/Task/TaskDetailView.swift
@@ -14,55 +14,37 @@ struct TaskDetailView: View {
     
     let task: Task
     var body: some View {
-        GeometryReader { geometry in
-            ZStack{
-                Color.gray700.ignoresSafeArea()
-                VStack(alignment:.leading){
-                    //1. Button : back button
-                    HStack{
-                        Button{
-                            dismiss()
-                        } label:{
-                            Image(systemName: "chevron.left")
-                                .foregroundColor(.green100)
-                                .font(.system(size:22))
-                            Text("보관함")
-                                .font(.system(size:17))
-                        }
-                    }.foregroundColor(.green100)
-                        .padding(.horizontal, geometry.size.width * 0.0203)
-                        .padding(.bottom, geometry.size.height * 0.0093)
-                    
-                    //.padding(8)
-                    //2. header: 작업 세부 정보
-                    HStack{
-                        Text("작업 세부 정보")
-                            .foregroundColor(.gray100)
-                            .font(.system(size: 28, weight: .bold))
-                        Spacer()
+        ZStack{
+            Color.gray700.ignoresSafeArea()
+            VStack(alignment:.leading){
+                //1. Button : back button
+                HStack{
+                    Button{
+                        dismiss()
+                    } label:{
+                        Image(systemName: "chevron.left")
+                            .foregroundColor(.green100)
+                            .font(.system(size:22))
+                        Text("보관함")
+                            .font(.system(size:17))
                     }
-                    .padding(.horizontal, geometry.size.width * 0.0407)
-                    .padding(.bottom, geometry.size.height * 0.0187)
-                    
-                    //3. Content
-                    
-                    VStack(alignment: .leading){
-                        Text("제목")
-                            .font(.system(size: 15))
-                            .foregroundColor(.gray300)
-                            .padding(.horizontal,geometry.size.width * 0.0381)
+                }.foregroundColor(.green100).padding(8)
+                //2. header: 작업 세부 정보
+                HStack{
+                    Text("작업 세부 정보")
+                        .foregroundColor(.gray100)
+                        .font(.system(size: 28, weight: .bold))
+                    Spacer()
+                }.padding(16)
+                
+                //3. Content
+                
+                VStack(alignment: .leading){
+                    Text("제목")
+                        .font(.system(size: 15))
+                        .foregroundColor(.gray300)
+                        .padding(.horizontal,15)
                         
-                        
-                        Text("서강대학교 홍보영상 기획 회의")
-                            .foregroundColor(.white)
-                            .padding(.horizontal,geometry.size.width * 0.0407)
-                            .font(.system(size:16, weight: .semibold))
-                            .frame(width: 368, height: 44, alignment: .leading)
-                            .background(.gray600)
-                            .cornerRadius(8)
-                        
-                    }.padding(.horizontal,geometry.size.width * 0.0330)
-                        .padding(.vertical,geometry.size.height * 0.0117)
                     
                     Text(task.title)
                         .foregroundColor(.white)
@@ -72,17 +54,16 @@ struct TaskDetailView: View {
                         .background(.gray600)
                         .cornerRadius(8)
                         
+                }.padding(.horizontal,13)
+                    .padding(.vertical,10)
+                
+                
+                VStack(alignment: .leading){
+                    Text("소요시간")
+                        .font(.system(size: 15))
+                        .foregroundColor(.gray300)
+                        .padding(.horizontal,15)
                         
-                        Text("3시간")
-                            .foregroundColor(.white)
-                            .padding(.horizontal,geometry.size.width * 0.0407)
-                            .font(.system(size:16, weight: .semibold))
-                            .frame(width: 368, height: 44, alignment: .leading)
-                            .background(.gray600)
-                            .cornerRadius(8)
-                        
-                    }.padding(.horizontal,geometry.size.width * 0.0330)
-                        .padding(.vertical,geometry.size.height * 0.0117)
                     
                     Text(task.time)
                         .foregroundColor(.white)
@@ -92,17 +73,15 @@ struct TaskDetailView: View {
                         .background(.gray600)
                         .cornerRadius(8)
                         
+                }.padding(.horizontal,13)
+                    .padding(.vertical,10)
+                
+                VStack(alignment: .leading){
+                    Text("작업 가능 시작일")
+                        .font(.system(size: 15))
+                        .foregroundColor(.gray300)
+                        .padding(.horizontal,15)
                         
-                        Text("May 28, 2025")
-                            .foregroundColor(.white)
-                            .padding(.horizontal,geometry.size.width * 0.0407)
-                            .font(.system(size:16, weight: .semibold))
-                            .frame(width: 368, height: 44, alignment: .leading)
-                            .background(.gray600)
-                            .cornerRadius(8)
-                        
-                    }.padding(.horizontal,geometry.size.width * 0.0330)
-                        .padding(.vertical,geometry.size.height * 0.0117)
                     
                     Text("\(task.startDate.formatted(date:.long, time:.omitted))")
                         .foregroundColor(.white)
@@ -112,17 +91,15 @@ struct TaskDetailView: View {
                         .background(.gray600)
                         .cornerRadius(8)
                         
+                }.padding(.horizontal,13)
+                    .padding(.vertical,10)
+                
+                VStack(alignment: .leading){
+                    Text("작업 마감일")
+                        .font(.system(size: 15))
+                        .foregroundColor(.gray300)
+                        .padding(.horizontal,15)
                         
-                        Text("Jun 5, 2025")
-                            .foregroundColor(.white)
-                            .padding(.horizontal,geometry.size.width * 0.0407)
-                            .font(.system(size:16, weight: .semibold))
-                            .frame(width: 368, height: 44, alignment: .leading)
-                            .background(.gray600)
-                            .cornerRadius(8)
-                        
-                    }.padding(.horizontal,geometry.size.width * 0.0330)
-                        .padding(.vertical,geometry.size.height * 0.0117)
                     
                     Text("\(task.endDate.formatted(date: .long, time:.omitted))")
                         .foregroundColor(.white)
@@ -132,17 +109,15 @@ struct TaskDetailView: View {
                         .background(.gray600)
                         .cornerRadius(8)
                         
+                }.padding(.horizontal,13)
+                    .padding(.vertical,10)
+                
+                VStack(alignment: .leading){
+                    Text("하루 중 선호 시간대")
+                        .font(.system(size: 15))
+                        .foregroundColor(.gray300)
+                        .padding(.horizontal,15)
                         
-                        Text("9:00 - 18:00")
-                            .foregroundColor(.white)
-                            .padding(.horizontal,geometry.size.width * 0.0407)
-                            .font(.system(size:16, weight: .semibold))
-                            .frame(width: 368, height: 44, alignment: .leading)
-                            .background(.gray600)
-                            .cornerRadius(8)
-                        
-                    }.padding(.horizontal,geometry.size.width * 0.0330)
-                        .padding(.vertical,geometry.size.height * 0.0117)
                     
                     Text(task.preferredTime)
                         .foregroundColor(.white)
@@ -152,26 +127,33 @@ struct TaskDetailView: View {
                         .background(.gray600)
                         .cornerRadius(8)
                         
-                    } label:{
-                        Text("가능한 시간 다시 보기")
-                            .foregroundColor(.green700)
-                            .font(.system(size:17, weight: .semibold))
-                            .frame(width: 361, height: 46, alignment: .center)
+                }.padding(.horizontal,13)
+                    .padding(.vertical,10)
+                
+                
+                
+                //4. Button
+                Button{
+                    
+                } label:{
+                    Text("가능한 시간 다시 보기")
+                        .foregroundColor(.green700)
+                        .font(.system(size:17, weight: .semibold))
+                        .frame(width: 361, height: 46, alignment: .center)
                             .background(.green200)
                             .cornerRadius(8)
-                            .padding(.horizontal, geometry.size.width * 0.0407)
-                        
-                        
-                    }
-                    .padding(.top,geometry.size.height * 0.0352)
-                    
-                    
-                    Spacer()
-                    
+                            .padding(.horizontal, 16)
+                            
                     
                 }
-            }.navigationBarBackButtonHidden()
-        }
+                    .padding(.top,30)
+                    
+                
+                Spacer()
+                
+                
+            }
+        }.navigationBarBackButtonHidden()
     }
 }
 
